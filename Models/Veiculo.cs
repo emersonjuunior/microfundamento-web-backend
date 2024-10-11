ffec906c5691708ea7b80e063757a745584e4506
Emerson Junior <emerson.c.junior.115@gmail.com>
2024-10-11 17:10:46 -0300

    Adicionando propriedades a classe Veículos

--- Models/Veiculo.cs
+++ Models/Veiculo.cs
@@ -1,6 +1,26 @@
-﻿namespace microfundamento_web_backend.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace microfundamento_web_backend.Models
 {
+    [Table("Veiculos")]
     public class Veiculo
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O campo de Nome é obrigatório!")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "O campo de Placa é obrigatório!")]
+        public string Placa { get; set; }
+
+        [Required(ErrorMessage = "O campo de Ano de Fabricação é obrigatório!")]
+        public int AnoFabricacao { get; set; }
+
+        [Required(ErrorMessage = "O campo de Ano do Modelo é obrigatório!")]
+        public int AnoModelo { get; set; }
+        public int AnoServicio { get; set; }
+
     }
 }
