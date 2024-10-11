34dcd7d734c77c65451f9081122f72b833c6f123
Emerson Junior <emerson.c.junior.115@gmail.com>
2024-10-11 17:33:57 -0300

    Configurações do Entity Framework

---
+++ Models/AppDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace microfundamento_web_backend.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public DbSet<Veiculo> Veiculos { get; set; }
+    }
+}
