bcb8fff33aba602f93e9beb6a6ed36636c2fa75f
Emerson Junior <emerson.c.junior.115@gmail.com>
2024-10-14 16:06:55 -0300

    Criação do método de adicionar veículos.

--- Controllers/VeiculosController.cs
+++ Controllers/VeiculosController.cs
@@ -1,6 +1,7 @@
 ﻿using microfundamento_web_backend.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace microfundamento_web_backend.Controllers
 {
@@ -17,6 +18,51 @@ namespace microfundamento_web_backend.Controllers
             var dados = await _context.Veiculos.ToListAsync();
 
             return View(dados);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(Veiculo veiculo)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Veiculos.Add(veiculo);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(veiculo);
+        }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var dados = await _context.Veiculos.FindAsync(id);
+
+            if(dados == null)
+                return NotFound();
+
+            return View(dados);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, Veiculo veiculo)
+        {
+            if (id != veiculo.Id)
+                return NotFound();
+
+            if (ModelState.IsValid)
+            {
+                _context.Veiculos.Update(veiculo);
+         
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View();
         }
     }
 }
---
+++ Migrations/20241014181430_RemoveAnoServicio.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace microfundamento_web_backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveAnoServicio : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AnoServicio",
+                table: "Veiculos");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "AnoServicio",
+                table: "Veiculos",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
